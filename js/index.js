f637be595ee86421a9ec1f232ee2088b3a340397
Gregoire Brechenmacher <greg.brech@hotmail.fr>
2018-01-25 11:35:20 +0100

    avancée

--- js/index.js
+++ js/index.js
@@ -1,9 +1,31 @@
 $(function(){
-        alert("toto");
-        setInterval(function(){
-            $(".slideshow ul").animate({marginLeft:-350},800,function(){
-                $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
-            })
-        }, 3500);
+
+    var num_img = 1;
+
+    function counter(next){
+        switch (num_img) {
+            case 1:
+                next ? num_img++ : num_img = 3;
+                break;
+            case 2:
+                next ? num_img++ : num_img--;
+                break;
+            case 3:
+                next ? num_img = 1 : num_img--;
+                break;
+        }
+
+        $('#cpt_img').text(num_img + '/3');
+    }
+
+    $('#previous').on('click', function() {
+        counter(false);
+        $('body').css({ 'background-image': 'url(Resources/slide-image-' + num_img + '.jpg)' });
+    });
+
+    $('#next').on('click', function() {
+        counter(true);
+        $('body').css({ 'background-image': 'url(Resources/slide-image-' + num_img + '.jpg)' });
+    });
 
 });
